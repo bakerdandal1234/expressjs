487c3fb86567b6af1c42217341fdea8e24176bbb
bakerdandal1234 <bakerdandal4@gmail.com>
2024-11-29 20:07:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
